dee6352d7bdab8fb9452172f977eae14271d90b8
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-01-12 23:15:03 -0500

    Added Login Component + Logic for login. Login, stay logged in, and logout work.

---
+++ client/src/Components/Login.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Login({ handleLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleClick() {
+    console.log("Login button has been clicked!");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => handleLogin(user));
+      }
+    });
+  }
+
+  return (
+    <div>
+      <div className="login-bckg">
+        <div className="signContainer">
+          <div className="stack"></div>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="username"
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="username"
+            />
+            <br />
+            <input
+              className="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="password"
+            />
+            <br />
+            <button onClick={handleClick} className="signIn" type="submit">
+              Sign In
+            </button>
+          </form>
+
+          {/* <Link to="/SignUp" className="signUp">
+            <h5>Don't have an account yet? Sign up.</h5>
+          </Link> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
