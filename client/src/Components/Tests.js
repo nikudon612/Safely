19ad9fa373ca49d20a266469a26f422aafc24122
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-01-15 19:19:57 -0500

    Began populating tests page w information. still needs to be finished

---
+++ client/src/Components/Tests.js
@@ -0,0 +1,37 @@
+function Tests() {
+  return (
+    <div className="testbox">
+      {/* <h1>hello papi</h1> */}
+      <div className="testcard">
+        <div className="testtitle">TYPES OF COVID TESTS AND WHAT THEY DO</div>
+        <br />
+        <div className="testcontent">
+          PCR
+          <br />
+          <br />
+          PCR tests (or “molecular tests”) are the gold standard in COVID-19
+          testing. They’re the most widely available and reliable type of test.
+          See more...
+          <br />
+          <br />
+          ANTIGEN
+          <br />
+          <br />
+          Antigen tests (or “rapid tests”) are usually available at emergency
+          locations. They provide results within minutes but tend to be less
+          accurate than their counterparts. See more...
+          <br />
+          <br />
+          ANTIBODY
+          <br />
+          <br />
+          Antibody tests detect to the presence of antibodies that countered
+          SARS-CoV-2 in the patients body. This is the type of test you should
+          take if you want to know whether you’ve had COVID-19 in the past. See
+          more...
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Tests;
